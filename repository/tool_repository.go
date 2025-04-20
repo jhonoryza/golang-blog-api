5616b631a0ead3cf3dfb698895622cc2c91475b6
fajar sidik priatna <jardik.oryza@gmail.com>
2025-04-20 10:59:33 +0700

    typo

--- repository/tool_repository.go
+++ repository/tool_repository.go
@@ -40,7 +40,7 @@ func FindAllTools(ctx context.Context, db *sql.DB) *[]entity.Tool {
 	args = append(args, typeBy)
 
 	if search != "" {
-		query += ` AND (lower(name) LIKE lower($2) OR lower(link) LIKE lower($2) OR lower(tools.desc) LIKE lower($3))`
+		query += ` AND (lower(name) LIKE lower($2) OR lower(link) LIKE lower($2) OR lower(tools.desc) LIKE lower($2))`
 		args = append(args, "%"+search+"%", "%"+search+"%")
 	}
 	query += " ORDER BY " + sortBy + " " + sortDir
